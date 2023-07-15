57727c0b8758902a23ae741e9bf9b8cf9896e00a
Wail Abou <abou.w@hotmail.com>
2023-07-15 16:23:10 +0200

    Update Switch.svelte

--- src/lib/components/ui/switch/Switch.svelte
+++ src/lib/components/ui/switch/Switch.svelte
@@ -9,6 +9,7 @@
     export let id: string | undefined = undefined;
     export let disabled: boolean = false;
     export let defaultChecked: boolean = false;
+    export let required: boolean = false;
 
     let checked: boolean = defaultChecked;
 
@@ -18,6 +19,7 @@
     }
 </script>
 
-<button {id} {disabled} on:click={toggle} aria-checked="{checked}" data-state="{checked ? 'checked' : 'unchecked'}" type="button" role="switch" value="on" class="{cn("peer inline-flex h-[24px] w-[44px] shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input", className)}">
+<button {id} {disabled} on:click={toggle} aria-checked="{checked}" data-state="{checked ? 'checked' : 'unchecked'}" type="button" role="switch" value="on" class="{cn("peer inline-flex h-6 w-11 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input", className)}">
     <span data-state="{checked ? 'checked' : 'unchecked'}" class="pointer-events-none block h-5 w-5 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-5 data-[state=unchecked]:translate-x-0"></span>
 </button>
+<input {required} aria-hidden="true" tabindex="-1" type="checkbox" value="on" class="absolute pointer-events-none opacity-0 !m-0 h-6 w-11">
