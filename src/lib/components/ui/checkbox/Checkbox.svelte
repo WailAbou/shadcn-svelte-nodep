b48fb7d3e33a9c7f39e7f1c827ca7b056d1c5230
Wail Abou <abou.w@hotmail.com>
2023-08-10 08:43:22 +0200

    Prevent enter from activating checkbox

--- src/lib/components/ui/checkbox/Checkbox.svelte
+++ src/lib/components/ui/checkbox/Checkbox.svelte
@@ -19,9 +19,13 @@
         checked = !checked;
         dispatch("checkedChange", checked);
     }
+
+    function preventEnter(event: KeyboardEvent) {
+        if (event.key === "Enter") event.preventDefault();
+    }
 </script>
 
-<button on:click={toggle} {id} {disabled} aria-checked="{checked}" type="button" role="checkbox" value="on"
+<button on:click={toggle} on:keydown={preventEnter} {id} {disabled} aria-checked="{checked}" type="button" role="checkbox" value="on"
 class="{cn("peer h-4 w-4 shrink-0 rounded-sm border border-primary ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 aria-checked:bg-primary aria-checked:text-primary-foreground", className)}">
     {#if checked}
         <Check class="h-4 w-4" />
