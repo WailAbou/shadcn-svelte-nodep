2bef6c5ee63a07e08b3b28b66f35bd296b76af0d
Wail Abou <abou.w@hotmail.com>
2023-08-14 11:31:33 +0200

    Added Aspect Ratio Component

---
+++ src/lib/components/examples/AspectRatioExample.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { AspectRatio } from '$components/ui/aspect-ratio';
+	import { Options } from '$components/custom/options';
+	import { Button } from '$components/ui/button';
+
+	let index: number = 0;
+	let aspectRatios: number[] = [16 / 9, 2 / 1, 1 / 2, 1 / 1];
+	let aspectRatiosDisplay: string[] = ['16 / 9', '2 / 1', '1 / 2', '1 / 1'];
+</script>
+
+<Options>
+	<Button on:click={() => (index = 0)}>Reset</Button>
+	<Button on:click={() => (index = (index + 1) % aspectRatios.length)}>Aspect Ratio: {aspectRatiosDisplay[index]}</Button>
+</Options>
+
+<AspectRatio ratio={aspectRatios[index]} class="bg-muted">
+	<img src="https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80" alt="Gray by Drew Beamer" class="h-full w-full rounded-md object-cover" />
+</AspectRatio>
