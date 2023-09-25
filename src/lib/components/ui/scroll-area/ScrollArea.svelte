26ac463fb524b1d01bd5886ac800e881b76db8b9
Wail Abou <abou.w@hotmail.com>
2023-09-25 13:47:26 +0200

    Update ScrollArea.svelte

--- src/lib/components/ui/scroll-area/ScrollArea.svelte
+++ src/lib/components/ui/scroll-area/ScrollArea.svelte
@@ -5,20 +5,12 @@
 	let className: string | undefined | null = undefined;
 	export { className as class };
 
-	// let scrollAreaHeight = 0;
-	// let scrollbarHeight = 0;
-
-	// let isDragging = false;
-	// let initialMouseY = 0;
-	// let initialScrollbarY = 0;
-
-	// let scrollbarY = 0;
-	// let init = false;
-
 	let scrollArea: HTMLDivElement;
 	let scrollbar: HTMLDivElement;
 	let scrollBarHeight = 40;
 	let scrollHeight = 0;
+	let isDragging = false;
+	let initialMouseY = 0;
 
 	const updateScrollbarHeight = () => {
 		let max = scrollArea.scrollHeight - scrollArea.clientHeight;
@@ -26,45 +18,26 @@
 		scrollHeight = percentage * (scrollArea.clientHeight - scrollBarHeight);
 	};
 
-	// onMount(() => {
-	// 	updateScrollbarHeight();
-	// 	window.addEventListener('resize', updateScrollbarHeight);
-	// 	init = true;
-	// });
+	const handleMouseDown = (event: MouseEvent) => {
+		isDragging = true;
+		initialMouseY = event.clientY;
+	};
 
-	// afterUpdate(() => {
-	// 	updateScrollbarHeight();
-	// });
+	const handleMouseMove = (event: MouseEvent) => {
+		if (isDragging) {
+			const deltaY = event.clientY - initialMouseY;
+			console.log(deltaY);
+			scrollArea.scrollTop = deltaY;
+		}
+	};
 
-	// const handleMouseDown = (event: MouseEvent) => {
-	// 	isDragging = true;
-	// 	initialMouseY = event.clientY;
-	// 	initialScrollbarY = scrollbarY;
-	// };
+	const handleMouseUp = () => {
+		isDragging = false;
+	};
 
-	// const handleMouseMove = (event: MouseEvent) => {
-	// 	if (isDragging) {
-	// 		const deltaY = event.clientY - initialMouseY;
-	// 		scrollbarY = Math.max(0, Math.min(initialScrollbarY + deltaY, scrollAreaHeight - scrollbarHeight));
-	// 	}
-	// };
-
-	// const handleMouseUp = () => {
-	// 	isDragging = false;
-	// };
-
-	// const handleMouseLeave = () => {
-	// 	isDragging = false;
-	// };
-
-	// $: {
-	// 	if (init) {
-	// 		const scrollArea = document.querySelector('[data-scroll-area-viewport]');
-	// 		if (scrollArea) {
-	// 			scrollArea.scrollTop = (scrollbarY / scrollAreaHeight) * scrollArea.scrollHeight;
-	// 		}
-	// 	}
-	// }
+	const handleMouseLeave = () => {
+		isDragging = false;
+	};
 </script>
 
 <div dir="ltr" class={cn('relative overflow-hidden', className)}>
@@ -73,20 +46,12 @@
 			<slot />
 		</div>
 	</div>
-	<!-- <div
-		data-orientation="vertical"
-		data-state="visible"
-		class="absolute right-0 top-0 flex h-full w-2.5 touch-none select-none border-l border-l-transparent p-[1px] transition-colors"
-		style="bottom: {0};transform: translateY({scrollbarY}px);"
+	<div
+		bind:this={scrollbar}
 		on:mousedown={handleMouseDown}
 		on:mousemove={handleMouseMove}
 		on:mouseup={handleMouseUp}
 		on:mouseleave={handleMouseLeave}
-	>
-		<div data-state="visible" class="relative flex-1 rounded-full bg-border" style="height: {scrollbarHeight}px;"></div>
-	</div> -->
-	<div
-		bind:this={scrollbar}
 		data-orientation="vertical"
 		data-state="visible"
 		class="absolute right-0 top-0 flex h-full w-2.5 touch-none select-none border-l border-l-transparent p-[1px] transition-colors"
