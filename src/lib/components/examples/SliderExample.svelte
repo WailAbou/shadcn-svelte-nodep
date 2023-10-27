36c71bb206e55fdb6b8a73f425b3a579e31c0e4b
Wail Abou <abou.w@hotmail.com>
2023-10-27 15:58:30 +0200

    Slider Component

--- src/lib/components/examples/SliderExample.svelte
+++ src/lib/components/examples/SliderExample.svelte
@@ -2,4 +2,5 @@
 	import { Slider } from '$components/ui/slider';
 </script>
 
-<Slider defaultValue={50} max={100} step={1} class={'w-[60%]'} />
+<Slider class={'w-[60%]'} />
+<!-- <Slider defaultValue={50} max={100} step={1} class={'w-[60%]'} /> -->
--- src/lib/components/ui/slider/Slider.svelte
+++ src/lib/components/ui/slider/Slider.svelte
@@ -3,23 +3,34 @@
 
 	let className: string | undefined | null = undefined;
 	export { className as class };
-	export let defaultValue: number;
+	export let defaultValue: number = 0;
 	export let max: number = 100;
 	export let min: number = 0;
-	export let step: number = 1;
-	export let inverted: boolean = false;
+	export let step: number = 30;
 	export let disabled: boolean = false;
 
+	let [scrollArea, scrollHandle]: HTMLDivElement[] = [];
 	let value: number = defaultValue;
+	let isDragging = false;
+
+	const updateValue = (event: MouseEvent) => {
+		if (isDragging) {
+			const start = scrollArea.getBoundingClientRect().x;
+			const end = start + scrollArea.clientWidth;
+			const newX = Math.min(Math.max(event.clientX, start), end);
+			const percentage = Math.round(((newX - start) / (end - start)) * 100);
+			value = percentage;
+		}
+	};
 </script>
 
-<!-- <svelte:window on:mousemove={syncAreaToScrollBar} on:mouseup={() => (isDragging = false)} /> -->
+<svelte:window on:mousemove={updateValue} on:mouseup={() => (isDragging = false)} />
 
 <span dir="ltr" data-orientation="horizontal" aria-disabled="false" class={cn('relative flex touch-none select-none items-center', className)}>
-	<span data-orientation="horizontal" class="relative h-2 w-full grow overflow-hidden rounded-full bg-secondary">
-		<span data-orientation="horizontal" class="absolute h-full bg-primary" style="left: 0%; right: 50%;"></span>
+	<span bind:this={scrollArea} data-orientation="horizontal" class="relative h-2 w-full grow overflow-hidden rounded-full bg-secondary">
+		<span data-orientation="horizontal" class="absolute left-0 h-full bg-primary" style="right: {100 - value}%;"></span>
 	</span>
-	<span style="left: calc(50% + 0px);" class="absolute translate-x-[-50%]">
+	<span bind:this={scrollHandle} style="left: {value}%" class="absolute translate-x-[-50%]" on:mousedown={() => (isDragging = true)}>
 		<span
 			aria-disabled={disabled}
 			role="slider"
