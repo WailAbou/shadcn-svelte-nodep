3673459051b4adc46e8c8dc12e6e02503d6bec9f
Wail Abou <abou.w@hotmail.com>
2023-10-28 13:53:06 +0200

    Improved Slider Component

--- src/lib/components/examples/SliderExample.svelte
+++ src/lib/components/examples/SliderExample.svelte
@@ -1,6 +1,31 @@
 <script lang="ts">
 	import { Slider } from '$components/ui/slider';
+	import { Options } from '$components/custom/options';
+	import { Button } from '$components/ui/button';
+
+	let value: number;
+	let disabled: boolean = false;
+	$: step = steps[stepIndex];
+	$: range = ranges[rangeIndex];
+
+	let steps: number[] = [1, 5, 20, 50];
+	let ranges: number[][] = [
+		[0, 100],
+		[10, 50],
+		[20, 40]
+	];
+	let [stepIndex, rangeIndex] = [0, 0];
+
+	$: uid = { disabled, step, range };
 </script>
 
-<Slider class={'w-[60%]'} />
-<!-- <Slider defaultValue={50} max={100} step={1} class={'w-[60%]'} /> -->
+<Options>
+	<p class="flex items-center justify-center">Value: {value}</p>
+	<Button on:click={() => (disabled = !disabled)}>Disabled: {disabled}</Button>
+	<Button on:click={() => (stepIndex = (stepIndex + 1) % steps.length)}>Step Size: {steps[stepIndex]}</Button>
+	<Button on:click={() => (rangeIndex = (rangeIndex + 1) % ranges.length)}>Range: {range[0]} - {range[1]}</Button>
+</Options>
+
+{#key uid}
+	<Slider on:valueChange={({ detail: newValue }) => (value = newValue)} defaultValue={(range[0] + range[1]) / 2} min={range[0]} max={range[1]} {step} {disabled} />
+{/key}
--- src/lib/components/ui/slider/Slider.svelte
+++ src/lib/components/ui/slider/Slider.svelte
@@ -1,48 +1,73 @@
 <script lang="ts">
 	import { cn } from '$lib/helpers/utils';
+	import { TestTube } from 'lucide-svelte';
+	import { createEventDispatcher } from 'svelte';
+
+	const dispatch = createEventDispatcher<{ valueChange: number }>();
 
 	let className: string | undefined | null = undefined;
 	export { className as class };
 	export let defaultValue: number = 0;
-	export let max: number = 100;
 	export let min: number = 0;
-	export let step: number = 30;
+	export let max: number = 100;
+	export let step: number = 1;
 	export let disabled: boolean = false;
 
-	let [scrollArea, scrollHandle]: HTMLDivElement[] = [];
-	let value: number = defaultValue;
-	let isDragging = false;
+	let value: number;
+	let isDragging: boolean = false;
+	let scrollArea: HTMLDivElement;
+	let handleX: number;
 
-	const updateValue = (event: MouseEvent) => {
+	const mappedDefaultValue = ((defaultValue - min) / (max - min)) * 100;
+	updateValues(mappedDefaultValue);
+
+	const onMouseMove = (event: MouseEvent) => {
 		if (isDragging) {
 			const start = scrollArea.getBoundingClientRect().x;
 			const end = start + scrollArea.clientWidth;
-			const newX = Math.min(Math.max(event.clientX, start), end);
-			const percentage = Math.round(((newX - start) / (end - start)) * 100);
-			value = percentage;
+			const absolutePercentage = ((event.clientX - start) / (end - start)) * 100;
+			updateValues(absolutePercentage);
 		}
 	};
+
+	function updateValues(value: number) {
+		const steppedPercentage = Math.round(value * (1 / step)) / (1 / step);
+		const clampedPercentage = Math.min(Math.max(steppedPercentage, 0), 100);
+		const mappedValue = Math.round((clampedPercentage / 100) * (max - min) + min);
+
+		handleX = clampedPercentage;
+		value = mappedValue;
+		dispatch('valueChange', value);
+	}
 </script>
 
-<svelte:window on:mousemove={updateValue} on:mouseup={() => (isDragging = false)} />
+<svelte:window on:mousemove={onMouseMove} on:mouseup={() => (isDragging = false)} />
 
-<span dir="ltr" data-orientation="horizontal" aria-disabled="false" class={cn('relative flex touch-none select-none items-center', className)}>
+<span
+	dir="ltr"
+	data-orientation="horizontal"
+	on:mousedown={(e) => {
+		isDragging = true;
+		onMouseMove(e);
+	}}
+	aria-disabled={disabled}
+	class={cn('relative flex touch-none select-none items-center aria-disabled:pointer-events-none aria-disabled:opacity-50', className)}
+>
 	<span bind:this={scrollArea} data-orientation="horizontal" class="relative h-2 w-full grow overflow-hidden rounded-full bg-secondary">
-		<span data-orientation="horizontal" class="absolute left-0 h-full bg-primary" style="right: {100 - value}%;"></span>
+		<span data-orientation="horizontal" class="absolute left-0 h-full bg-primary" style="right: {100 - handleX}%;"></span>
 	</span>
-	<span bind:this={scrollHandle} style="left: {value}%" class="absolute translate-x-[-50%]" on:mousedown={() => (isDragging = true)}>
+	<span style="left: {handleX}%" class="absolute translate-x-[-50%]">
 		<span
-			aria-disabled={disabled}
 			role="slider"
-			aria-valuemin={min}
-			aria-valuemax={max}
 			aria-orientation="horizontal"
 			data-orientation="horizontal"
 			tabindex="0"
+			aria-valuemin={min}
+			aria-valuemax={max}
 			aria-valuenow={value}
-			class="aria block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 aria-disabled:pointer-events-none aria-disabled:opacity-50"
+			class="aria block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2"
 		>
 		</span>
 	</span>
 </span>
-<input {value} class="hidden" />
+<input {value} class="hidden" {disabled} />
